e3321a60714be757b12279ae46bd59a0f541d333
Joe Huelsman <kcgooner91@gmail.com>
2020-05-06 20:54:43 -0500

    added user authentification

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 
 const PORT = process.env.PORT || 3000;
 
