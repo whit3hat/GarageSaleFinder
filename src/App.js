ef1643d81d6c1e7aa52ab08972ae46a8b60748cd
Spencer Wood <spencer.wood@me.com>
2020-05-14 18:12:13 -0500

    moved the main page js into the main.js page file,created the routes, moved all the js from the app.js file, created a main.css page and moved all the app.css info into that.  Need to fix the routes still

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import Main from '../src/pages/Main';
 // import { Link } from 'react-router-dom'
 // import Main from './Components/main';
 
@@ -9,21 +8,7 @@ import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 function App() {
     return (
         <div>
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="/landingpage">Garage Sale Finder</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="./Components/home.js">Home</Nav.Link>
-      <Nav.Link href="./Components/landingpage.js">Seeker</Nav.Link>
-      <Nav.Link href="./Components/modal.js">Log in or Sign up</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>          
+          <Main />
         </div>
 
     );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,25 @@
-import react from 'react';
-import Nav from './Nav';
+import React from 'react';
+import './main.css';
+import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
 function Main(){
-    <Nav />
-
+    return (
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="/landingpage">Garage Sale Finder</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="./Components/home.js">Home</Nav.Link>
+      <Nav.Link href="./Components/landingpage.js">Seeker</Nav.Link>
+      <Nav.Link href="./Components/modal.js">Log in or Sign up</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>  
+    )        
 };
 
 export default Main;
