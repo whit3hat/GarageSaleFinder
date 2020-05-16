48bfd733adef5e2c58d198796f919965ac4e30e4
Spencer Wood <spencer.wood@me.com>
2020-05-16 10:47:30 -0500

    removed  the double routes in the app.js file

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
     return (
       <Router>
         <div>
-        <Main />
-        <Signup />
+    
 
           <Route exact path = '/' component={Main} />
           <Route exact path = '/signup' component={Signup} />
