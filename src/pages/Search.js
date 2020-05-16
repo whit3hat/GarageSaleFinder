79ee1c9b682a42a0e2523553fec04d8c77576e46
Valyoung <valeshkayoungsang@yahoo.com>
2020-05-16 16:01:42 -0500

    added styling to the button and font

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.css';
-import { InputGroup, FormControl, Container, Col, Row } from 'react-bootstrap';
+import { InputGroup, FormControl, Container, Col, Row, Button } from 'react-bootstrap';
 
 
 function Searching(){
@@ -11,7 +11,7 @@ function Searching(){
 <InputGroup size="lg" className="search-box">
     <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
     <InputGroup.Prepend>
-      <InputGroup.Text id="inputGroup-sizing-lg">Search</InputGroup.Text>
+      <Button style={{background:'#00CED1	'}} id="inputGroup-sizing-lg">Search</Button>
     </InputGroup.Prepend>
   </InputGroup>
   </Col>
