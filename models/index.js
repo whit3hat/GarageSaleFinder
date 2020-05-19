bc8550e576df5abc94006d8deddbaa43526c9498
Spencer Wood <spencer.wood@me.com>
2020-05-18 19:25:50 -0500

    made Sales from sales on poster.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,5 @@
-module.exports = {poster : require ("./poster") , userModel : ("./userModel") , search : ("./search.js"), image : ("./image.js")};
+module.exports = {
+poster : require ("./poster") , 
+userModel : ("./userModel") , 
+search : ("./search.js"), 
+image : ("./image.js")};
