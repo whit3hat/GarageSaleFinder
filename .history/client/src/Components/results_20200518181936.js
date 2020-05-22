d0dbe94b4b63eda33f2af57efa4de24a0fe940dd
Joe Huelsman <kcgooner91@gmail.com>
2020-05-22 14:45:19 -0500

    removed history

--- .history/client/src/Components/results_20200518181936.js
+++
@@ -1,8 +0,0 @@
-var myLatlng = new google.maps.LatLng();
-var mapOptions = {
-  zoom: 8,
-  center: myLatlng,
-  mapTypeId: 'roadmap'
-};
-var map = new google.maps.Map(document.getElementById('map'),
-    mapOptions);
--- .history/client/src/pages/Results_20200522143738.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default {
-    getMap: function(query) {
-        return axios.get(URL + query )
-    }
-};
