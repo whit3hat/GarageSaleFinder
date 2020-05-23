24816b76f46426896bce7aedbb20a5fa0dab8e88
Spencer Wood <spencer.wood@me.com>
2020-05-22 22:38:43 -0500

    added the current location js file, added to the testmap.js file and modified the render to include current location

--- client/src/Components/map/testmap.js
+++ client/src/Components/map/testmap.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './CurrentLocation';
 
-const mapStyles = {
-  width: '50%',
-  height: '50%'
-};
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+onMarkerClick = (props, marker, e) =>
+  this.setState( {
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
+  
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      <CurrentLocation
+      centerAroundCurrentLocation
+      google={this.props.google}
+      >
+     <Marker onClick={this.onMarkerClick} name={'Current location'} />
+
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onMapClicked={this.onMapClicked}>
+
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div> 
+
+        </InfoWindow>
+        </CurrentLocation>
     );
   }
 }
