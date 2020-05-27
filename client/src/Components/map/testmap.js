e5d955b1754d86191828f02f351f33ac37d38a98
Spencer Wood <spencer.wood@me.com>
2020-05-26 20:19:42 -0500

    added the map css to the map folder and imported into the testmap.js file

--- client/src/Components/map/testmap.js
+++ client/src/Components/map/testmap.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import './style.css';
 import CurrentLocation from './CurrentLocation';
 // import { FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import API from '../../utils/API';
 
-// function geoLocation() {
-//   console.log('geoLocation function')
+function geoLocation() {
+  console.log('geoLocation function')
 
-//   //API call to get all sales from the DB
-//   API.getUser(console.log('getUser API call'))
-//   //    take the address from sign up fields and convert for DB
-//   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formObject.formStreetNumber}+${formObject.formStreetName}+${formObject.formCity}+${formObject.formState}&key=AIzaSyAQV4OnQ1CIWg_QP0L_yL1lsftxK5K6CUs`)
-//     .then(function (res) {
-//       console.log(res)
-//     })
-//     .catch(function (err) {
-//       console.log(err)
-//     })
-//     .then(function (res) {
+  //API call to get all sales from the DB
+  API.getUser()
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+  //    take the address from sign up fields and convert for DB
+  // axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${formObject.formStreetNumber}+${formObject.formStreetName}+${formObject.formCity}+${formObject.formState}&key=AIzaSyAQV4OnQ1CIWg_QP0L_yL1lsftxK5K6CUs`)
+  //   .then(function (res) {
+  //     console.log(res)
+  //   })
+  //   .catch(function (err) {
+  //     console.log(err)
+  //   })
+  //   .then(function (res) {
 
-//     });
-// }
+  //   });
+}
 
 export class MapContainer extends Component {
   state = {
@@ -34,7 +37,7 @@ onMarkerClick = (props, marker, e) =>
   this.setState( {
     selectedPlace: props,
     activeMarker: marker,
-    showingInfoWindow: true
+    showingInfoWindow: true,
   });
 
   onClose = (props) => {
@@ -50,6 +53,7 @@ onMarkerClick = (props, marker, e) =>
 
   render() {
     return (
+      
       <Map
       centerAroundCurrentLocation
       google={this.props.google}
